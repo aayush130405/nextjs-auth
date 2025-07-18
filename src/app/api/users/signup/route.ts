eee299e1c86e5f3b25a8f7fc9d85c4e2d0337508
Aayush <aayush130405@gmail.com>
2025-07-18 13:31:22 +0530

    update signup frontend and add signup api

---
+++ src/app/api/users/signup/route.ts
@@ -0,0 +1,45 @@
+import {connect} from "@/dbConfig/dbConfig"         //importing connect function to connect DB
+import User from "@/models/userModel"
+import { NextRequest, NextResponse } from "next/server"     //similar to req, res of express
+import bcryptjs from "bcryptjs"
+
+connect()       //connecting DB
+
+export async function POST(request: NextRequest) {
+    try {
+        const reqBody = await request.json()
+        const {username, email, password} = reqBody
+
+        console.log("Request body:", reqBody);
+
+        //check if user already exists
+        const user = await User.findOne({email})
+
+        if(user) {
+            return NextResponse.json({error: "User already exists"}, {status: 400}) 
+        }
+
+        //hash password
+        const salt = await bcryptjs.genSalt(10)
+        const hashedPassword = await bcryptjs.hash(password, salt)
+
+        //create new user with provided body
+        const newUser = new User({
+            username,
+            email,
+            password: hashedPassword
+        })
+
+        //saving created user in DB
+        const savedUser = await newUser.save()
+        console.log(savedUser);
+        
+        return NextResponse.json({
+            message: "Created user successfully",
+            success: true,
+            savedUser      
+        })
+    } catch (error: any) {
+        return NextResponse.json({error: error.message}, {status:500})
+    }
+}  
