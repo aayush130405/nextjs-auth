9550907f28c8afb1478bc028a7f2651d48367974
Aayush <aayush130405@gmail.com>
2025-07-20 09:50:27 +0530

    add logout functionality and backend

--- src/app/api/users/login/route.ts
+++ src/app/api/users/login/route.ts
@@ -37,9 +37,6 @@ export async function POST(request: NextRequest) {
         response.cookies.set("token", token, {httpOnly: true})
 
         return response
-
-
-        
     } catch (error: any) {
         return NextResponse.json({error: error.message}, {status: 500})
     }
