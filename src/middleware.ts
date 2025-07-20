dc10ae2b23b4577acc17b4302aa447b165a3a06d
Aayush <aayush130405@gmail.com>
2025-07-20 12:21:39 +0530

    add middleware.ts

---
+++ src/middleware.ts
@@ -0,0 +1,27 @@
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+
+export function middleware(request: NextRequest) {
+    const path = request.nextUrl.pathname       //current path we are at
+
+    const isPublicPath = path === '/login' || path === '/signup'        //checking if current path is public
+
+    const token = request.cookies.get('token')?.value || ''       //getting value of token
+
+    if(isPublicPath && token) {
+        return NextResponse.redirect(new URL('/', request.nextUrl))
+    }
+
+    if(!isPublicPath && !token) {
+        return NextResponse.redirect(new URL('/login', request.nextUrl))
+    }
+}
+
+export const config = {
+    matcher: [
+        '/',
+        '/profile/:path*',
+        '/login',
+        '/signup'
+    ]
+}
