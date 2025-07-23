75bd0c1edbf961a6f10b9618aa642deec88b8884
Aayush <aayush130405@gmail.com>
2025-07-23 20:46:00 +0530

    fix bugs and add type dependencies

--- src/app/profile/[id]/page.tsx
+++ src/app/profile/[id]/page.tsx
@@ -1,15 +1,12 @@
-type UserProfileProps = {
-    params: { id: string }
-};
-
-export default function UserProfile({params}: UserProfileProps) {
+export default async function UserProfile({params}: { params: Promise<{ id: string }> }) {
+    const { id } = await params;
     return (
         <div className="min-h-screen flex items-center justify-center bg-gradient-to-br from-gray-900 via-purple-900 to-violet-600">
             <div className="bg-white/90 shadow-2xl rounded-2xl p-10 w-full max-w-md flex flex-col items-center">
                 <h1 className="text-4xl font-bold text-center text-gray-800 mb-2 tracking-tight">Profile</h1>
                 <p className="text-center text-gray-500 mb-8 text-2xl">Profile page</p>
                 <span className="px-6 py-3 rounded-lg bg-gradient-to-r from-orange-400 to-pink-500 text-white font-bold text-lg shadow-md">
-                    {params.id}
+                    {id}
                 </span>
             </div>
         </div>
